bfefccf4fc18e4d185f0453424b6e23a8e42c4d1
Anas Magdy <anas0magdy@gmail.com>
2025-11-20 18:48:18 +0200

    Change testimonials to image screenshots

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,21 +6,22 @@ function Footer() {
     <footer className="site-footer-main">
       <div className="footer-content">
         <div className="footer-map">
+          {/* تم تحديث الخريطة لتشير إلى العنوان: 52 شارع أبو العتاهية - مدينة نصر */}
           <iframe 
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3453.666017300306!2d31.23340831511527!3d30.04646798188182!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x145840c8f1036c0d%3A0x60a87a8f1e6f4a3e!2sTahrir%20Square!5e0!3m2!1sen!2seg!4v1677777777777!5m2!1sen!2seg" 
             width="100%" 
             height="300" 
-            style={{ border: 0 }}
-            allowFullScreen="" 
-            loading="lazy" 
-            referrerPolicy="no-referrer-when-downgrade"
+            frameBorder="0" 
+            scrolling="no" 
+            marginHeight="0" 
+            marginWidth="0" 
+            src="https://maps.google.com/maps?q=52%20Abu%20El%20Atahiya%20St,%20Nasr%20City&t=&z=15&ie=UTF8&iwloc=&output=embed"
             title="موقع العيادة"
           ></iframe>
         </div>
         <div className="footer-info">
           <h3>د. هـمـس سـامـي</h3>
           <p>
-            العنوان: (يرجى إضافة العنوان هنا)
+            العنوان: 52 شارع أبو العتاهية، امتداد عباس العقاد، مدينة نصر، القاهرة.
           </p>
           <p>
             رقم الهاتف: 
@@ -34,7 +35,7 @@ function Footer() {
         </div>
       </div>
       <div className="footer-bottom">
-        <p>جميع الحقوق محفوظة © {new Date().getFullYear()} Anas Magdy</p>
+        <p>جميع الحقوق محفوظة © {new Date().getFullYear()} د. همس سامي</p>
       </div>
     </footer>
   );
