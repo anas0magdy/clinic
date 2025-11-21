3c4c2f3e170f2787bc6c7131884f91ebd47516c3
Anas Magdy <anas0magdy@gmail.com>
2025-11-21 21:18:14 +0200

    version_2

--- src/components/FloatingSocials.jsx
+++ src/components/FloatingSocials.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import './FloatingSocials.css';
 import { Phone, WhatsApp, Facebook, Instagram } from './Icons';
+import { useLanguage } from '../context/LanguageContext';
 
 function FloatingSocials() {
+  const { toggleLanguage, lang } = useLanguage(); 
+
   const phoneNumber = '+201065957501';
   const whatsappNumber = '201065957501';
   
@@ -11,6 +14,15 @@ function FloatingSocials() {
   
   return (
     <div className="floating-container">
+      {/* زر اللغة العائم (فوق الفيسبوك) */}
+      <button 
+        onClick={toggleLanguage} 
+        className="floating-icon language-float-btn"
+        aria-label="Change Language"
+      >
+        {lang === 'ar' ? 'En' : 'ع'}
+      </button>
+
       <a 
         href={facebookLink} 
         className="floating-icon facebook"
@@ -30,7 +42,7 @@ function FloatingSocials() {
         <Instagram />
       </a>
       <a 
-        href={`https://api.whatsapp.com/send?phone=${whatsappNumber}`}
+        href={`https://wa.me/${whatsappNumber}`}
         className="floating-icon whatsapp"
         target="_blank"
         rel="noopener noreferrer"
